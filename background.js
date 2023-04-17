f5e8e6b287041f7037f7b7edd342ce54fb433236
Ashwin Kumar <ashwinncj@gmail.com>
2023-04-18 01:03:10 +0530

    Updated fies for context menu.

--- background.js
+++ background.js
@@ -1,10 +1,12 @@
 chrome.contextMenus.create({
     id: "generate-funnny-tweet",
-    title: "Generate a Funny Tweet",
+    title: "Generate a Funny Tweet for \"%s\"",
     contexts: ["selection"]
 });
 
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
-    chrome.action.setPopup({tabId: tab.id, popup: 'popup.html'});
-    chrome.action.openPopup();
+    if (info.menuItemId === "generate-funnny-tweet") {
+        chrome.action.setPopup({tabId: tab.id, popup: 'popup.html'});
+        chrome.action.openPopup();
+    }
 });
