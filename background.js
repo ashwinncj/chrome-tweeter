86f763641ad7c7e7a66907347c593813dcd0445b
Ashwin Kumar <ashwinncj@gmail.com>
2023-04-18 23:38:10 +0530

    Updated code to get tweets.

--- background.js
+++ background.js
@@ -14,25 +14,20 @@ function generateAndUpdate(tab,selection){
 
     chrome.storage.sync.get('api_key', function(data) {
         const api_key = data.api_key;
-        var suggestion = getTweetSuggestions(selection, api_key);
-        chrome.storage.local.set({ tweets: suggestion });
-        chrome.action.setPopup({tabId: tab.id, popup: 'popup.html'});
-        chrome.action.openPopup();
+        getTweetSuggestions(selection, api_key, tab);
     });
 }
 
-function getTweetSuggestions(selection, api_key){
-    const url = 'https://api.openai.com/v1/completions';
-    const prompt = 'Write a funny tweet for the following text:\n' + selection +'\n\nTweet:';
+function getTweetSuggestions(selection, api_key, tab){
+    const url = 'https://api.openai.com/v1/chat/completions';
+    const prompt = 'Write a very funny tweet with 100 characters for this -:\n' + selection;
 
     const data = {
         "model": "gpt-3.5-turbo",
-        "prompt": prompt,
-        "max_tokens": 50,
-        "n": 1,
-        "stream": false,
-        "logprobs": null,
-        "stop": "\n"
+        "messages": [
+            {"role":"user", "content": prompt}
+        ],
+        "max_tokens": 50
       };
   
     var headers = {
@@ -50,10 +45,16 @@ function getTweetSuggestions(selection, api_key){
     return fetch(url, requestOptions)
       .then((response) => response.json())
       .then((data) => {
-        // const tweet = data.choices[0].text.trim();
-        return data;
+        const tweet = data.choices[0].message.content;
+        showTweetSuggestion(tweet, tab);
       })
       .catch((error) => {
         console.error(error);
       });
+}
+
+function showTweetSuggestion(tweet, tab) {
+    chrome.storage.local.set({ tweets: tweet });
+    chrome.action.setPopup({tabId: tab.id, popup: 'popup.html'});
+    chrome.action.openPopup();
 }
